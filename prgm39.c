7afaf6572229deace324df900fe28b6faf2b2d22
reshmiranjani <50790684+reshmiranjani@users.noreply.github.com>
2019-05-21 20:13:42 +0530

    Create prgm39.c

---
+++ prgm39.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+    int n,i,j,arr[50],temp;
+    for(i=0;i<10;i++)
+    {
+      scanf("%d",&arr[i]);
+    }
+    for(i=0;i<10;i++)
+    {
+       for(j=i+1;j<10;j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    printf("%d",temp);
+}
+    
