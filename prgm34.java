0ee86ca3221dc083d161aab7848fe1ade072bd6c
reshmiranjani <50790684+reshmiranjani@users.noreply.github.com>
2019-05-21 23:39:10 +0530

    Create prgm34.java

---
+++ prgm34.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+ public class Main
+ {
+     public static void main(String args[])
+     {
+         String a;
+         int count=0;
+         Scanner sc=new Scanner(System.in);
+         a=sc.nextLine();
+         char n[]=a.toCharArray();
+         for( int i=0;i<n.length;i++)
+         {
+             if(n[i]=='.')
+             {
+                 count++;
+             }
+         }
+         System.out.print(count+1);
+     }
+ }
